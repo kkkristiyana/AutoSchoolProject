bd94d44bac70c8e2002421fceeb24b7837778950
kkkristiyana <kknikolova_@abv.bg>
2026-01-14 14:48:06 +0200

    Add project files.

---
+++ AutoSchoolProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AutoSchoolProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
