e2fc0e6593a5d1d09371d7c4169ff61330712adb
kkkristiyana <kknikolova_@abv.bg>
2026-04-29 00:20:30 +0300

    edit course admin panel fix

--- AutoSchoolProject/ViewModels/Admin/CourseCrudViewModels.cs
+++ AutoSchoolProject/ViewModels/Admin/CourseCrudViewModels.cs
@@ -16,15 +16,17 @@ namespace AutoSchoolProject.ViewModels.Admin
     {
         public int Id { get; set; }
 
-        [Required, StringLength(100)]
+        [Required(ErrorMessage = "Името е задължително.")]
+        [StringLength(100, ErrorMessage = "Името не може да бъде по-дълго от 100 символа.")]
         [Display(Name = "Име")]
         public string Name { get; set; } = string.Empty;
 
-        [Range(typeof(decimal), "0.01", "100000")]
+        [Required(ErrorMessage = "Цената е задължителна.")]
+        [Range(typeof(decimal), "0,01", "100000", ErrorMessage = "Цената трябва да бъде по-голяма от 0.")]
         [Display(Name = "Цена (лв)")]
         public decimal Price { get; set; }
 
-        [Range(1, 200)]
+        [Range(1, 200, ErrorMessage = "Броят задължителни часове трябва да е между 1 и 200.")]
         [Display(Name = "Задължителни часове практика")]
         public int RequiredPracticeLessons { get; set; } = 31;
     }
